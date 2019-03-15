6be36bc207192c5fb81f38aa5d8d5ace94b326df
unknown <hamid.r_torabi@yahoo.com>
2019-03-15 19:20:58 +0330

    Adds pdf And word

--- TestBloodData.java
+++
@@ -1,8 +0,0 @@
-package com.maktab25.hw4.torabi.practice4;
-
-public class TestBloodData {
-    public static void main(String[] args) {
-        BloodData bloodData=new BloodData("ab",'-');
-        System.out.println(bloodData.getBloodType());
-    }
-}
